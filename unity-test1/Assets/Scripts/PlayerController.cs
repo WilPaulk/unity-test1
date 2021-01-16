25bd66ec277e88b4e6f4d9a215bec0d445f337f2
WilPaulk <wil.paulk@gmail.com>
2021-01-16 11:13:50 -0500

    Simple movement and sprite creation

---
+++ unity-test1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dirX = Input.GetAxisRaw("Horizontal");
+        float dirY = Input.GetAxisRaw("Vertical");
+
+        gameObject.transform.position = new Vector2 (transform.position.x + (dirX * speed), 
+            transform.position.y + (dirY * speed));
+    }
+
+
+}
