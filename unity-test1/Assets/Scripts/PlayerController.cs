b8edf3c7e5a765e36fa2ad4d3e3a6ce24971ee5a
WilPaulk <wil.paulk@gmail.com>
2021-01-18 11:05:00 -0500

    Added notes to PlayerController

--- unity-test1/Assets/Scripts/PlayerController.cs
+++ unity-test1/Assets/Scripts/PlayerController.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Unity components
     Animator animator;
     Rigidbody2D rb2d;
     SpriteRenderer spriteRenderer;
     BoxCollider2D boxCollider2D;
 
+    // Grounded checks
+    [SerializeField] bool isGrounded;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private Transform groundCheckL;
+    [SerializeField] private Transform groundCheckR;
+    [SerializeField] private float groundCheckRadius;
+    [SerializeField] LayerMask groundedLayerMask;
+    
+    // Non-binary movement variables
     [SerializeField] private float movementSpeed = 4f;
     [SerializeField] private float sprintSpeed = 7f;
     [SerializeField] private float jumpHeight = 5f;
@@ -16,26 +26,18 @@ public class PlayerController : MonoBehaviour
     private float horizontalSpeed;
     private int extraJumpsRemaining;
 
-// Grounded checks
-    [SerializeField] bool isGrounded;
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private Transform groundCheckL;
-    [SerializeField] private Transform groundCheckR;
-    [SerializeField] private float groundCheckRadius;
-    [SerializeField] LayerMask groundedLayerMask;
-
-// Binary movement variables
+    // Binary movement variables
     private bool rightMovement;
     private bool leftMovement;
     private bool groundJump;
     private bool extraJump;
     private bool posVelocity;
-    [SerializeField] private bool dropThrough;
-    [SerializeField] private bool dropThroughCoroutineIsRunning;
+    private bool dropThrough;
+    private bool dropThroughCoroutineIsRunning;
 
     void Start()
     {
-        // Initialize components
+        // Get components
         animator = GetComponent<Animator>();
         rb2d = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
@@ -58,48 +60,35 @@ public class PlayerController : MonoBehaviour
         Jump();        
     }
 
-    private void Move()
+    private void Move() // Check for movement input
     {
+        // Standard movement
         rightMovement = Input.GetKey("d") || Input.GetKey("right") ? true : false;
         leftMovement = Input.GetKey("a") || Input.GetKey("left") ? true : false;
         if (Input.GetKeyDown("space") && isGrounded) 
             groundJump = true;
         if (Input.GetKeyDown("space") && !isGrounded && extraJumpsRemaining > 0)
             extraJump = true;
+
         // Sprint logic
         if (Input.GetKeyDown("left shift") && isGrounded)
             horizontalSpeed = sprintSpeed;
         if (Input.GetKeyUp("left shift"))
             horizontalSpeed = movementSpeed;
+        
+        // Platform handling
         dropThrough = Input.GetKey("s") ? true : false;
     }
 
-    private void GroundedCheck()
+    private void GroundedCheck() // Check if player is grounded
     {
-        // float extraHeightTest = 0.1f;
-        // RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, Vector2.down, extraHeightTest, groundedLayerMask);
-        // Color rayColor;
-        // if (raycastHit.collider != null)
-        // {
-        //     rayColor = Color.green;
-        // }
-        // else
-        // {
-        //     rayColor = Color.red;
-        // }
-        // Debug.DrawRay(boxCollider2D.bounds.center + new Vector3(boxCollider2D.bounds.extents.x,0), Vector2.down * (boxCollider2D.bounds.extents.y + extraHeightTest), rayColor);
-        // Debug.DrawRay(boxCollider2D.bounds.center - new Vector3(boxCollider2D.bounds.extents.x,0), Vector2.down * (boxCollider2D.bounds.extents.y + extraHeightTest), rayColor);
-        // Debug.DrawRay(boxCollider2D.bounds.center - new Vector3(boxCollider2D.bounds.extents.x, boxCollider2D.bounds.extents.y + extraHeightTest), Vector2.right * (boxCollider2D.bounds.extents.x) * 2, rayColor);
-        // Debug.Log(raycastHit.collider);
-        // isGrounded = raycastHit.collider != null;
-
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundedLayerMask) ||
             Physics2D.OverlapCircle(groundCheckL.position, groundCheckRadius, groundedLayerMask) ||
             Physics2D.OverlapCircle(groundCheckR.position, groundCheckRadius, groundedLayerMask);
         posVelocity = rb2d.velocity.y >= 0.0f ? true : false;
     }
 
-    void OnDrawGizmosSelected()
+    void OnDrawGizmosSelected() // Debugging vizualization for GroundedCheck()
     {
         Gizmos.color = (isGrounded) ? Color.green : Color.red;
         Gizmos.DrawSphere(groundCheck.position, groundCheckRadius);
@@ -107,7 +96,8 @@ public class PlayerController : MonoBehaviour
         Gizmos.DrawSphere(groundCheckR.position, groundCheckRadius);
     }
 
-    private void BaseDirectionalMovement()
+    // Standard directional movement physics & animation handling
+    private void BaseDirectionalMovement() 
     {
         if (!isGrounded)
         {
@@ -137,7 +127,7 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private void Jump()
+    private void Jump() // Jump movement physics
     {
         if (groundJump)
         {
@@ -152,6 +142,8 @@ public class PlayerController : MonoBehaviour
             extraJump = false;
         }
     }
+
+    // Create relationship between player/platforms for natural platform movement
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Platform") && !posVelocity && isGrounded)
@@ -160,6 +152,7 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    // Intialize DropThrough() coroutine if called while on platform
     private void OnCollisionStay2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Platform") && isGrounded)
@@ -169,9 +162,9 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    // Coroutine to temporarily ignore collision between player and platforms
     IEnumerator DropThrough()
     {
-        Debug.Log("start");
         dropThroughCoroutineIsRunning = true;
         GameObject[] platformArray = GameObject.FindGameObjectsWithTag("Platform");
         foreach (GameObject platform in platformArray)
@@ -184,9 +177,9 @@ public class PlayerController : MonoBehaviour
             Physics2D.IgnoreCollision(boxCollider2D,platform.GetComponent<BoxCollider2D>(),false);
         }
         dropThroughCoroutineIsRunning = false;
-        Debug.Log("end");
     }
 
+    // Decouple player and platform upon exiting collision
     private void OnCollisionExit2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Platform"))
@@ -195,18 +188,3 @@ public class PlayerController : MonoBehaviour
         }
     }
 }
-
-// Debug logs for groundedCheck:
-        // Color rayColor;
-        // if (raycastHit.collider != null)
-        // {
-        //     rayColor = Color.green;
-        // }
-        // else
-        // {
-        //     rayColor = Color.red;
-        // }
-        // Debug.DrawRay(boxCollider2D.bounds.center + new Vector3(boxCollider2D.bounds.extents.x,0), Vector2.down * (boxCollider2D.bounds.extents.y + extraHeightTest), rayColor);
-        // Debug.DrawRay(boxCollider2D.bounds.center - new Vector3(boxCollider2D.bounds.extents.x,0), Vector2.down * (boxCollider2D.bounds.extents.y + extraHeightTest), rayColor);
-        // Debug.DrawRay(boxCollider2D.bounds.center - new Vector3(boxCollider2D.bounds.extents.x, boxCollider2D.bounds.extents.y + extraHeightTest), Vector2.right * (boxCollider2D.bounds.extents.x), rayColor);
-        // Debug.Log(raycastHit.collider);
